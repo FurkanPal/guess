010fac15666e9de0276db0079dc014898fb3b836
Ata <fpala74@gmail.com>
2024-04-01 00:37:01 +0200

    underScore parts about to finish

--- systemDesign.java
+++ systemDesign.java
@@ -1,12 +1,31 @@
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class systemDesign{
-    public void system() {
-        WriteNew writeNew = new WriteNew();
+    WriteNew writeNew = new WriteNew();
+    
+    public void startGame(String underScores) {
         Scanner scanner = new Scanner(System.in);
-        writeNew.write("What your command sir?");
-        String userInput = scanner.nextLine();
-        writeNew.write("your command is: " + userInput);
-        scanner.close();
+        final Logger logger = Logger.getLogger(Main.class.getName());
+        writeNew.write("Start game?(Y/N)");
+        String command = scanner.nextLine();
+        if (command.equals("N") ) {
+        } else {
+            int match = 4;
+            int life = 5;
+            while (life != 0) {
+                life -= 1;
+                System.out.println(underScores);
+                scanner.nextLine();
+            } scanner.close(); 
+        } 
+    }
+    public String undersCorePrinter(String word){
+        StringBuilder result = StringBuilder();
+        System.out.println(word.length());
+        for (int i = 0; i < word.length(); i++){
+            result.append("_ ");
+        }
+        return result;
     }
 }
